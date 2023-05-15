386417960c645b98cd9304350b74f4d5b4aa8c7d
memesnopower <115490701+memesnopower@users.noreply.github.com>
2023-05-15 22:32:26 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,32 +1,45 @@
+#include <Account.h>
 #include <Transaction.h>
 #include <gtest/gtest.h>
 
+TEST(Account, Banking){
+	Account test(0,0); //constructor
+	ASSERT_EQ(test.GetBalance(), 0); 
+	// lock test
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error); // add balance
+	test.Lock(); //lock
+	ASSERT_NO_THROW(test.ChangeBalance(100)); //can we change?
+//change balance, check balance
+	ASSERT_EQ(test.GetBalance(), 100);
+//if it locked?
+	ASSERT_THROW(test.Lock(), std::runtime_error);
+//check unlock
+	test.Unlock();
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+}
+
 TEST(Transaction, Banking){
 	const int base_A = 5000, base_B = 5000, base_fee = 100;
-	//тестовые объекты
-	Account x(0,base_A), y(1,base_B);
+	Account Man1(0,base_A), Man2(1,base_B);
 	Transaction tran;
-	//конструкторы и т п
+//constructors
 	ASSERT_EQ(tran.fee(), 1);
 	tran.set_fee(base_fee);
 	ASSERT_EQ(tran.fee(), base_fee);
-	//ошибка транзакции
-	ASSERT_THROW(tran.Make(x, x, 1000), std::logic_error);
-	ASSERT_THROW(tran.Make(x, y, -50), std::invalid_argument);
-	ASSERT_THROW(tran.Make(x, y, 50), std::logic_error);
-	if (tran.fee()*2-1 >= 100) {
-		ASSERT_EQ(tran.Make(x, y, tran.fee()*2-1), false);
-	}
-	//что все верно лочится
-	x.Lock();
-	ASSERT_THROW(tran.Make(x, y, 1000), std::runtime_error);
-	x.Unlock();
-	//транзакция проходит
-	ASSERT_EQ(tran.Make(x, y, 1000), true);
-	ASSERT_EQ(y.GetBalance(), base_B+1000);
-	ASSERT_EQ(x.GetBalance(), base_A-1000-base_fee);
-	//недостаточно денег
-	ASSERT_EQ(tran.Make(x, y, 3900), false);
-	ASSERT_EQ(y.GetBalance(), base_B+1000);
-	ASSERT_EQ(x.GetBalance(), base_A-1000-base_fee);
+//when transaction doesnt work
+	ASSERT_THROW(tran.Make(Man1, Man1, 1000), std::logic_error);
+	ASSERT_THROW(tran.Make(Man1, Man2, -50), std::invalid_argument);
+	ASSERT_THROW(tran.Make(Man1, Man2, 50), std::logic_error);
+	if (tran.fee()*2-1 >= 100)
+		ASSERT_EQ(tran.Make(Man1, Man2, tran.fee()*2-1), false);
+//lock
+	Man1.Lock();
+	ASSERT_THROW(tran.Make(Man1, Man2, 1000), std::runtime_error);
+	Man1.Unlock();
+	ASSERT_EQ(tran.Make(Man1, Man2, 1000), true); //all good
+	ASSERT_EQ(Man2.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Man1.GetBalance(), base_A-1000-base_fee);
+	ASSERT_EQ(tran.Make(Man1, Man2, 3900), false);//not enough money
+	ASSERT_EQ(Man2.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Man1.GetBalance(), base_A-1000-base_fee);
 }
